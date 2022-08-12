91651e8df41fdd8e77b76ccd9e2927c47e96afb3
Vaishnavi429 <guptavaish429@gmail.com>
2022-08-12 12:04:06 +0530

    mui textfield add

--- src/Component/AddNewPatient/AddNewPatient.js
+++ src/Component/AddNewPatient/AddNewPatient.js
@@ -44,11 +44,7 @@ export default function AddNewPatient() {
                 .required('Age must be fill'),
             sex: Yup.string()
                 .required('sex must be fill'),
-            // IDDetail: Yup.string().when('IDtype',{
-            //     is:(IDtype:string)
-            // }
-            
-            //   ),
+           
              
         }),
 
